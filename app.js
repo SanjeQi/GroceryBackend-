6bd78cae44f2fbdf8b9a3944a0476deccc079195
github username <github email address>
2019-01-18 13:27:27 +0000

    setUp node dep

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const db = require('./db/db.js')
+// Set up the express app
+const app = express()
+// get all todos
+app.get('/api/v1/todos', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    todos: db
+  })
+})
+const PORT = 5000
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
