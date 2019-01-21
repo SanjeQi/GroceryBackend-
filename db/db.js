3c2933d9c1a44fcb410c0fc18b481ced5e8d8608
github username <github email address>
2019-01-21 09:28:32 +0000

    express up and running

--- db/db.js
+++ db/db.js
@@ -3,6 +3,31 @@ const todos = [
     id: 1,
     title: 'lunch',
     description: 'Go for lunc by 2pm'
+  },
+  {
+    id: 2,
+    title: 'dinner',
+    description: '20:00 pm'
+  },
+  {
+    id: 3,
+    title: 'breakfast',
+    description: 'Go for lunc by 8Am'
+  },
+  {
+    id: 4,
+    title: 'snack',
+    description: 'Go for snack by 4pm'
+  },
+  {
+    id: 5,
+    title: 'fruits',
+    description: 'Go for fruits by 3pm'
+  },
+  {
+    id: 6,
+    title: 'church',
+    description: 'every Day'
   }
 ]
 
